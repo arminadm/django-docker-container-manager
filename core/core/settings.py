09dcd22689bb56d3e5def0caf0067516941ebfad
arminadm <armindarabimahboub@gmail.com>
2022-12-05 19:17:26 +0330

    swagger applied to the project

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project'
+    'project',
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- core/core/urls.py
+++ core/core/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-urlpatterns = [
+schema_view = get_schema_view(
+    openapi.Info(
+        title="blog API",
+        default_version="v1",
+        description="api documentation for blog",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="armindarabimahboub@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [ 
     path('admin/', admin.site.urls),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path('', include("project.api.v1.urls")),
 ]
